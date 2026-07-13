a0883f8ae6a26b03ad4cc0275a8a9383eaf33cda
Mangalesh S <181747208+MangaleshSaravanan@users.noreply.github.com>
2026-07-13 07:30:14 +0530

    Refactor login function to include user levels

--- BMS_backend/auth.py
+++ BMS_backend/auth.py
@@ -1,5 +1,12 @@
-import secrets
+from secrets import token_hex
 
-def login():
-    return
-
+def login(level,userid,pwd):
+    if level=='admin':
+        exist=check_admin_table(userid,pwd)
+        if exist:
+            return 'adm-'+token_hex(16)+''
+    elif level=='customer':
+        exist=check_customer_table(userid,pwd)
+        if exist:
+            return 'cus-'+token_hex(16)+''
+    return False
