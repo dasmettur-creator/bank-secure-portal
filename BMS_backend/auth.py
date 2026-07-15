d8379fe045226fb66b5e172791957d560ff1f2b1
Mangalesh S <181747208+MangaleshSaravanan@users.noreply.github.com>
2026-07-15 06:45:08 +0530

    Refactor login function to use check_table

--- BMS_backend/auth.py
+++ BMS_backend/auth.py
@@ -1,12 +1,10 @@
 from secrets import token_hex
-
+from database import check_table
 def login(level,userid,pwd):
-    if level=='admin':
-        exist=check_admin_table(userid,pwd)
-        if exist:
+        exist=check_table(level,userid,pwd)
+        if exist and level=='admin':
             return 'adm-'+token_hex(16)+''
-    elif level=='customer':
-        exist=check_customer_table(userid,pwd)
-        if exist:
+        elif exist and level=='customer':
             return 'cus-'+token_hex(16)+''
-    return False
+        return False
+
