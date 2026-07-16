baabdf26c1769e3b9def89e98550b8a928369164
Mangalesh S <181747208+MangaleshSaravanan@users.noreply.github.com>
2026-07-16 06:54:02 +0530

    Enhance adminMenu layout with new frames and buttons
    Refactor adminMenu class to include content frame and buttons.

--- BMS_interface/admin_menu.py
+++ BMS_interface/admin_menu.py
@@ -1,17 +1,44 @@
 from customtkinter import *
-set_appearance_mode("dark")
-set_default_color_theme("blue")
 
 class adminMenu(CTk):
     def __init__(self):
         super().__init__()
-        self.menu=CTkFrame(self)
         self.title("Admin Menu - Bank of Paradise - Secure Portal")
         self.geometry("1920x1080")
-        self.menu.grid(row=0,column=0,sticky="nsew")
         self.grid_rowconfigure(0,weight=1)
         self.grid_columnconfigure(0,weight=1)
-        self.menu.grid_columnconfigure((0,1),weight=1)
+        self.grid_columnconfigure(1,weight=3)
+
+        self.menu=CTkFrame(self,fg_color="white")
+        self.menu.grid(row=0,column=0,sticky="nsew")
+        self.menu.grid_columnconfigure(0,weight=1)
+        self.menu.grid_rowconfigure((0,1,2,3,4,5,6,7),weight=1)
+
+        self.content=CTkFrame(self,fg_color="black")
+        self.content.grid(row=0,column=1,sticky="nsew")
+        self.content.grid_rowconfigure((0,1,2,3,4,5,6,7,8,9),weight=1)
+        self.content.grid_columnconfigure((0,1),weight=1)
+
+        CTkLabel(self.menu,text="Admin menu",font=("Jokerman",40),text_color="black").grid(row=0,column=0,sticky="nsew")
+        button=["Dashboard", "Customers", "Accounts", "KYC", "Transactions", "Reports", "Settings"]
+        for item in button:
+            btn = CTkButton(
+               self.menu,
+               text=item,
+               font=("Helvetica",18,"bold"),
+               height=50,
+               hover_color="green",
+               width=150,
+               corner_radius=25,
+               border_color="black",
+               text_color="white",
+               fg_color="darkblue",
+               border_width=5,
+               command=lambda page=item: self.menu_clicked(page)
+               ).grid(pady=8)
+
+    def menu_clicked(self,page):
+        pass
 
 if __name__=="__main__":
     app=adminMenu()
